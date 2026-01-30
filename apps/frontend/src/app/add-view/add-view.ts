2f61e9cc3e0ea1e5bc7ad045941dfc3e076d4eda
Duc Nhat Anh Nguyen <nducnhatanh@icloud.com>
2026-01-30 23:08:44 +0100

    Add selection between automatic and manual Anh

--- apps/frontend/src/app/add-view/add-view.ts
+++ apps/frontend/src/app/add-view/add-view.ts
@@ -1,12 +1,15 @@
 import { Component } from "@angular/core";
 import { AddExams } from "../add-exams/add-exams";
 import { AddExercises } from "../add-exercises/add-exercises";
+import { FormsModule } from '@angular/forms';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 @Component({
     selector: 'app-add-view',
     templateUrl: './add-view.html',
     styleUrls: ['./add-view.scss'],
-    imports: [AddExams, AddExercises],
+    imports: [AddExams, AddExercises, FormsModule, SelectButtonModule],
 }) export class AddView {
-
+    protected addMode: string = "automatic";
+    protected addModeSelectionOptions = [{label: "Automatic", value: "automatic"}, {label: "Manually", value: "manual"}] 
 }
